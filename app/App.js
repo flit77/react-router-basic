7b4db2b5220f0938077b1bc0d045e3593609220a
Vasiliy Shubko <flit77@gmail.com>
2017-03-25 22:48:45 +0100

    router setup

--- app/App.js
+++ app/App.js
@@ -1,4 +1,20 @@
 import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
-const App = () => <h1>Hello World!</h1>
+import { render } from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={Home} />
+          <Route path='/address' component={Address} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+const Home = () => <h1>Hello from Home!</h1>
+const Address = () => <h1>We are located at 555 Jackson St.</h1>
 export default App
