1fae1ced197e0a50c9117dd03d1073ee0b1fcbcc
Vasiliy Shubko <flit77@gmail.com>
2017-03-25 23:31:23 +0100

    not found router

--- app/App.js
+++ app/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom';
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path='/' component={Home} />
-          <Route path='/address' component={Address} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/address' component={Address} />
+            <Route component={NoMatch} />
+          </Switch>
         </div>
       </Router>
     )
@@ -17,4 +20,11 @@ class App extends Component {
 
 const Home = () => <h1>Hello from Home!</h1>
 const Address = () => <h1>We are located at 555 Jackson St.</h1>
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>No match for <code>{location.pathname}</code></h3>
+  </div>
+)
+
+
 export default App
